9df1158ddbbd50e6eef91c07b4ec99c0e1dfea77
Guru Charan <gururk2@gmail.com>
2022-07-20 14:48:48 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'plumb5Test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
